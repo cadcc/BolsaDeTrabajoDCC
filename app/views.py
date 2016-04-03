a27c4b214d192ac3a09e84a34724aacc3cef81af
emoreno-94 <edward.moreno@ing.uchile.cl>
2016-04-03 00:41:35 -0300

    Eliminacion de metodos demas

--- app/views.py
+++ app/views.py
@@ -2,16 +2,6 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-def prueba_1(request):
-    context = {'hola': 'texto para la primera vista'}
-    return render(request, 'prueba/vista_1.html', context)
-
-@csrf_exempt
-def prueba_2(request):
-    context = {'hola': 'texto para la segunda vista'}
-    return render(request, 'prueba/vista_2.html', context)
-
-@csrf_exempt
 def home(request):
     return render(request, 'app/home.html')
 
