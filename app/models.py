b89b14df9057afcb7a714d6f7a71c0d299f41c18
Felipe Rubilar <f_rubilar@outlook.com>
2016-04-08 17:19:36 -0300

    Tablas BD

--- app/models.py
+++ app/models.py
@@ -1,8 +1,32 @@
 from django.db import models
 
 # Create your models here.
-class PruebaLoca(models.Model):
-    nombre = models.CharField(max_length=24)
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=64)
+    apellido = models.CharField(max_length=64)
+    email = models.EmailField()
+    fecha = models.DateField()
+    roles = models.ManyToManyField(Rol)
 
-    def __unicode__(self):
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+
+class Rol(models.Model):
+    nombre = models.CharField(max_length=64)
+
+    def __str__(self):
         return self.nombre
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=64)
+    tipo = models.IntegerField()
+
+    def __str__(self):
+        return self.tipo + " - " + self.nombre
+
+class Suscripcion(models.Model):
+    periodicidad = models.IntegerField()
+    etiquetas = models.ManyToManyField(Etiqueta)
+
+    def __str__(self):
+        return self.periodicidad
--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
-    url(r'^company_offer_form/$', views.company_offer_form, name='company_offer_form'),
-    url(r'^offer/$', views.offer, name='offer'),
-    url(r'^offer_list/$', views.offer_list, name='offer_list')
+    url(r'^formulario/$', views.company_offer_form, name='formulario'),
+    url(r'^oferta/$', views.offer, name='oferta'),
+    url(r'^listado_ofertas/$', views.offer_list, name='listado_ofertas')
 ]
