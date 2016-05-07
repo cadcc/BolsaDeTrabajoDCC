660063128efa4b6912131895eecd32e6160ca509
emoreno-94 <edward.moreno@ing.uchile.cl>
2016-05-07 16:44:24 -0300

    Datos base para la base de datos...

---
+++ sql/rol.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.app_rol (id, nombre) VALUES (1, 'administrador');
+INSERT INTO public.app_rol (id, nombre) VALUES (2, 'publicador');
+INSERT INTO public.app_rol (id, nombre) VALUES (4, 'moderador');
+INSERT INTO public.app_rol (id, nombre) VALUES (3, 'validador');
+INSERT INTO public.app_rol (id, nombre) VALUES (5, 'normal');
+INSERT INTO public.app_rol (id, nombre) VALUES (6, 'pendiente');
