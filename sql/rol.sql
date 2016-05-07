ee4fce0edc2fc9a9ff415c5238f30b10e358a329
emoreno-94 <edward.moreno@ing.uchile.cl>
2016-05-07 17:07:52 -0300

    Sacados datos para eliminar inconsistencias

--- sql/rol.sql
+++ sql/rol.sql
@@ -1,6 +1,6 @@
-INSERT INTO public.app_rol (id, nombre) VALUES (1, 'administrador');
-INSERT INTO public.app_rol (id, nombre) VALUES (2, 'publicador');
-INSERT INTO public.app_rol (id, nombre) VALUES (4, 'moderador');
-INSERT INTO public.app_rol (id, nombre) VALUES (3, 'validador');
-INSERT INTO public.app_rol (id, nombre) VALUES (5, 'normal');
-INSERT INTO public.app_rol (id, nombre) VALUES (6, 'pendiente');
+INSERT INTO public.app_rol (nombre) VALUES ('administrador');
+INSERT INTO public.app_rol (nombre) VALUES ('publicador');
+INSERT INTO public.app_rol (nombre) VALUES ('moderador');
+INSERT INTO public.app_rol (nombre) VALUES ('validador');
+INSERT INTO public.app_rol (nombre) VALUES ('normal');
+INSERT INTO public.app_rol (nombre) VALUES ('pendiente');
